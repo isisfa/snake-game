178d66cbdc9a3e717ee7d307baebd4fe0732331a
isisfa <isis.falves@gmail.com>
2021-04-08 22:15:27 +0100

    define snake food randomness and increase

--- script.js
+++ script.js
@@ -63,7 +63,13 @@ function startGame () {
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    //increasing and decreasing snake
+    if(snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    }else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
